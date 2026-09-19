c9379883108bf8a0a591a3f05bd5891a85155374
hhmohd1230-wq <hhmohd1230@gmail.com>
2026-09-19 08:29:42 +0400

    Keep the local build helpers out of the repo

--- _push.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set "G=C:\Users\User\AppData\Local\GitHubDesktop\app-3.6.6\resources\app\git\cmd\git.exe"
-cd /d C:\Users\User\Documents\GitHub\UIW
-"%G%" add -A
-"%G%" commit -F _commitmsg.txt
-"%G%" push origin HEAD
-echo EXITCODE=%errorlevel%
